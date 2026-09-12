54a89b50b452b7b27249e28aef4e754c984f9ac7
anamnesismundi <anamnesismundi@gmail.com>
2026-09-12 21:37:16 +0300

    Publish anthropogony data cache token

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260912-public-material-cosmos-citations-1";
+const DATA_VERSION = "20260912-public-primordial-human-adam-psychic-citations-1";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
