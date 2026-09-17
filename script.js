08f83af3a64c2182c35016c0c9e94c0d725631de
anamnesismundi <anamnesismundi@gmail.com>
2026-09-17 20:29:55 +0300

    Refresh public Monad alias token

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260917-public-entry-clearance-1";
+const DATA_VERSION = "20260917-public-monad-alias-cleanup-1";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
