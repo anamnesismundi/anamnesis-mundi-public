c941f6be457b494aa4730b26be8bb91478a04f39
anamnesismundi <anamnesismundi@gmail.com>
2026-09-18 20:41:57 +0300

    Render Twelve Aeons contextual paragraph

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260918-public-aeon-structural-note-1";
+const DATA_VERSION = "20260918-public-aeon-structural-paragraph-1";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
@@ -1538,6 +1538,10 @@ function createTwelveAeonsCard(
       </h2>
 
       ${summary}
+
+      ${group.contextualNote
+        ? `<p class="twelve-aeons-context-note">${escapeHtml(group.contextualNote)}</p>`
+        : ""}
 
       ${groupSources}
 
