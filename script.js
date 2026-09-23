5508138b47318a0d2f29994bf7b4088f6083a07d
anamnesismundi <anamnesismundi@gmail.com>
2026-09-23 17:01:30 +0300

    Synchronize mobile source wrapping data token

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260923-public-mobile-prose-widow-control-2";
+const DATA_VERSION = "20260923-public-mobile-source-wrapping-3";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
