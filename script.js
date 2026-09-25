a5f55c10686c4912d8b39b71e4f78c69cb7262c6
anamnesismundi <anamnesismundi@gmail.com>
2026-09-25 16:16:48 +0300

    Render the approved Yaldabaoth note as a spaced paragraph

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260925-public-demiurge-title-2";
+const DATA_VERSION = "20260925-public-yaldabaoth-note-1";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
@@ -1139,6 +1139,12 @@ function createEntityCard(
       entity.summary || ""
     );
 
+  const contextualNote =
+    createSummaryMarkup(
+      entity.contextualNote || "",
+      "event-contextual-note"
+    );
+
   const pleromaticProcess =
     createPleromaticProcessMarkup(
       entity
@@ -1183,6 +1189,7 @@ function createEntityCard(
       </h2>
 
       ${summary}
+      ${contextualNote}
 
       ${alternativeNames}
       ${sources}
