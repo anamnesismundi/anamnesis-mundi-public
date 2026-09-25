0c16487af11f1f23944cece12b9a567d90914616
anamnesismundi <anamnesismundi@gmail.com>
2026-09-25 16:24:14 +0300

    Refresh data token for Yaldabaoth source citation

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260925-public-yaldabaoth-note-1";
+const DATA_VERSION = "20260925-public-yaldabaoth-note-2";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
