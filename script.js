ae2ebcdb6f5091bd94de4e1083f62901f739c70d
anamnesismundi <anamnesismundi@gmail.com>
2026-09-25 17:15:19 +0300

    Refresh Yaldabaoth entity card data

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260925-public-yaldabaoth-card-3";
+const DATA_VERSION = "20260925-public-yaldabaoth-entity-copy-4";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
