21847d6088eb89e00dca566efe9b1217eee1238f
anamnesismundi <anamnesismundi@gmail.com>
2026-09-25 16:03:40 +0300

    Refresh chronology data token for Demiurge title

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ window.addEventListener(
 );
 
 const DATA_PATH = "data/";
-const DATA_VERSION = "20260925-public-demiurge-title-1";
+const DATA_VERSION = "20260925-public-demiurge-title-2";
 
 const HTML_ENTITIES = {
   "&": "&amp;",
